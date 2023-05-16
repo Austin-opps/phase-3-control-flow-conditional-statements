ecaa9af37cdb2afe5eccb17a7bc7b8565963e6de
Austin <akomen04@gmail.com>
2023-05-16 13:25:07 +0300

    Completed assignments

--- control_flow.rb
+++ control_flow.rb
@@ -22,11 +22,11 @@ end
 
 def fizzbuzz(num)
   # your code here
-  if num % 3 === 0 && num % 5 === 0
+  if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
-  elsif num % 3 === 0
+  elsif num % 3 == 0
     "Fizz"
-  elsif num % 5 === 0
+  elsif num % 5 == 0
     "Buzz"
   else
     num
